1229b958161b2fcba03bf93613dc6a9fc900d096
Guichly Hessen <guichly.hessen@gmail.com>
2017-11-07 21:48:03 +0200

    Update asennus.sh

--- SETUP/asennus.sh
+++ SETUP/asennus.sh
@@ -1,3 +1,6 @@
+#Suomen näppäimistö käyttöönotto 
+setxkbmap fi
+
 #Alku järjestelyt ja asennukset
 sudo apt-get update
 sudo apt install -y git
@@ -6,7 +9,7 @@ sudo apt install -y puppet
 #Projektin kloonaus 
 git clone https://github.com/guichly/puppet-tests.git
 
-#lisää 9 riviin "ordering = manifest".
+#lisää 9 riviin "ordering = manifest". https://www.gnu.org/software/sed/manual/sed.html
 sudo sed -i "9i ordering = manifest" /etc/puppet/puppet.conf
 
 #kopioidaan rekursiivisesti "-r" ja päällekirjoittamatta samat nimiset "-n" tiedostot /etc/ hakemistoon.
@@ -17,6 +20,3 @@ sudo puppet apply -e 'class {'openssh':}'
 
 #Linuxin ajan hallinnan demoni (Suomen aikaan) https://www.systutorials.com/docs/linux/man/1-timedatectl/
 sudo timedatectl set-timezone Europe/Helsinki
-
-#Suomen näppäimistö käyttöönotto 
-setxkbmap fi
