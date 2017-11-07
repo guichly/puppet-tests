8fbd0245e271a95296226e20dc8379543cbc230d
Guichly Hessen <guichly.hessen@gmail.com>
2017-11-07 08:51:59 -0800

    setup files

---
+++ puppet/modules/openssh/manifests/init.pp
@@ -0,0 +1,22 @@
+class openssh {
+	  #paketin asennus
+	package { ssh:
+	ensure => 'installed',
+	  #livetikussa puppet komennon suorittamista varten.
+	#allowcdrom => true,
+	}	
+
+	  #ssh konfiguraatio tiedostojen muokkaaminen.
+	file { '/etc/ssh/ssh_config':
+	content => template("openssh/ssh_config"),
+	  #servicen huomautus sshd_config tiedoston muuttuessa
+	notify => Service["ssh"],
+	}
+
+	  #ssh moduulin toiminnan varmistaminen.
+	service { 'ssh':
+	ensure => 'running',
+	enable => 'true',
+	}
+}
+
